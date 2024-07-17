429bc6275096d1aef75099a9569fb9ceef08f041
Simon125q <106029626+Simon125q@users.noreply.github.com>
2024-07-17 23:06:24 +0200

    impl. detecting types of markdown blocks

--- src/block_conversions.py
+++ src/block_conversions.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
+class BlockType(Enum):
+    paragraph = "paragraph"
+    heading = "heading"
+    code = "code"
+    quote = "quote"
+    unordered_list = "unordered_list"
+    ordered_list = "ordered_list"
 
 def markdown_to_blocks(text: str) -> list[str]:
     lines = text.split("\n")
@@ -13,4 +22,26 @@ def markdown_to_blocks(text: str) -> list[str]:
     if curr_block != "":
         blocks.append(curr_block.strip())
     return blocks
+
+def get_block_type(block: str) -> BlockType:
+    if block[:2] == "# ":
+        return BlockType.heading
+    elif block[:3] == "```" and block[-3:] == "```":
+        return BlockType.code
+    lines = block.split("\n")
+    if lines[0][:2] in ["* ", "- "]:
+        is_un_list = True
+        for line in lines:
+            if line[:2] not in ["* ", "- "]:
+                is_un_list = False
+        if is_un_list:
+            return BlockType.unordered_list
+    elif lines[0][:3] == "1. ":
+        for index, line in enumerate(lines):
+            if not line.startswith(str(index + 1) + ". "):
+                return BlockType.paragraph
+        return BlockType.ordered_list
+
+    return BlockType.paragraph
+    
 
--- src/test_block_conversions.py
+++ src/test_block_conversions.py
@@ -1,5 +1,5 @@
 import unittest
-from block_conversions import markdown_to_blocks 
+from block_conversions import markdown_to_blocks, get_block_type, BlockType 
 
 class TestBlockConversions(unittest.TestCase):
     def test_split_blocks(self) -> None:
@@ -19,3 +19,27 @@ class TestBlockConversions(unittest.TestCase):
                     ]
         self.assertEqual(result2, expected2)
 
+    def test_get_block_type(self) -> None:
+        input1 = "# Some heading"
+        result1 = get_block_type(input1)
+        self.assertEqual(result1, BlockType.heading)
+
+        input2 = "#Some not heading"
+        result2 = get_block_type(input2)
+        self.assertEqual(result2, BlockType.paragraph)
+
+        input3 = "``` Some code block\nwith a lots of code\n```"
+        result3 = get_block_type(input3)
+        self.assertEqual(result3, BlockType.code)
+
+        input4 = "* unordered\n* list\n- that starts with\n* *"
+        result4 = get_block_type(input4)
+        self.assertEqual(result4, BlockType.unordered_list)
+
+        input5 = "1. ordered\n2. list\n3. that starts with\n4. this"
+        result5 = get_block_type(input5)
+        self.assertEqual(result5, BlockType.ordered_list)
+
+        input6 = "1. ordered\n2. list\n4. that starts with\n5. this"
+        result6 = get_block_type(input6)
+        self.assertEqual(result6, BlockType.paragraph)
